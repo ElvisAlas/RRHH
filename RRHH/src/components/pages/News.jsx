1732cbbd23aab7e8383d28c1ff19f77125c3ec52
ElvisAlas <110005376+ElvisAlas@users.noreply.github.com>
2024-03-20 13:46:38 -0600

    creando menu

---
+++ RRHH/src/components/pages/News.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavbarHook from '../NavbarHook/NavbarHook.jsx'
+import  "../NavbarHook/NavbarHook.css";
+
+
+const News = () => {
+
+  return (
+
+    <div className="main-content">
+        <NavbarHook />
+      <p>News</p>
+    </div>
+  );
+};
+
+export default News;
